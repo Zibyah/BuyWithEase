9fc3d63feb2c8641fde4a34745c4668a301fce49
Emetila <emetila83@gmail.com>
2024-04-04 08:23:20 +0100

    Created the files and pages needed for the development

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './app/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootDOM = document.getElementById('root');
+const root = ReactDOM.createRoot(rootDOM);
 root.render(
   <React.StrictMode>
     <App />
